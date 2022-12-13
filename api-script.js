b60b034287986cdd52b1bec230b9b83dca8db74e
MindyZHAOMinzhu <zhaominzhumindy@gmail.com>
2022-12-13 17:55:49 +0000

    update to V&A page

--- api-script.js
+++ api-script.js
@@ -10,12 +10,42 @@ request.onload = function() {
     var parsedData = JSON.parse(response);
     //console.log(parsedData);
 
-    var imageUrl = parsedData.data.images.original.url;
-    //console.log(imageUrl);
 
-    imageTag = "<img src=" + imageUrl + "  width='500'>";
-    //console.log(imageTag);
+
+    // // images CODE!!!
+    // var imageUrl = parsedData.data.images.original.url;
+    // //console.log(imageUrl);
+
+    // imageTag = "<img src=" + imageUrl + "  width='500'>";
+    // //console.log(imageTag);
+    // document.getElementById("holder").innerHTML = imageTag;
+
+
+    // V&A CODE!!!
+    // API gives as much as it can
+
+    // console.log(parsedData.records);
+    parsedData.records.forEach(record => {
+        /* ------------ Smaller Images ------------*/
+        // if(record._primaryImageId != null){
+        // 	 console.log(record);
+        // 	imageUrl = record._images._primary_thumbnail;
+
+        // 	imageTag += "<img src='" + imageUrl + "'  >";
+        // }
+        /* ----------------------------------------*/
+
+        /* ------------ Larger Images ------------*/
+        if (record._primaryImageId != null) {
+            console.log(record);
+            imageUrl = record._images._iiif_image_base_url;
+
+            imageTag += "<img src='" + imageUrl + "full/!300,300/0/default.jpg'  >";
+        }
+    });
     document.getElementById("holder").innerHTML = imageTag;
+
+
 };
 
 request.send();
